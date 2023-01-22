6cd5350d3484c42af2d6e29027f52afc966b284c
anodatwin <michalwalencik@o2.pl>
2023-01-22 22:09:54 +0100

    wyswitlenie wynikowych scian

--- git.py
+++ git.py
@@ -18,3 +18,12 @@ for w in walls:
 print(f'Liczba ścian zewnętrznych: {len(ext_walls)}') 
 
 totalvolume = 0 
+
+for w in ext_walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    for psetname, pset_dict in psets.items():
+        for name, value in pset_dict.items():
+            # print (f"{name}: {value}")
+            if name == "NetVolume":
+                totalvolume += float(value)
+print(f'TotalVolume: {totalvolume:.2f}')
