7bbc8008437ae728fb65276a17f6c417694e1edc
anodatwin <michalwalencik@o2.pl>
2023-01-22 22:01:29 +0100

    Zliczenie scian

--- git.py
+++ git.py
@@ -1,2 +1,8 @@
 import ifcopenshell.util.element
 import pprint as pp 
+
+m = ifcopenshell.open("231110AC-11-Smiley-West-04-07-2007.ifc")
+walls = m.by_type("IfcWall")
+print(len(walls))
+
+print(f'Liczba ścian w modelu: {len(walls)}')
